9b34e5a005ad5fe187e91e377568ed312a208b68
HubertKorcala <korcalahubert@gmail.com>
2023-04-26 11:38:31 +0200

    feat: add MockupContent

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Header from "./components/Header";
+import MockupContent from "./components/MockupContent";
 
 function App() {
   return (
     <>
       <Header />
+      <MockupContent />
     </>
   );
 }
---
+++ src/components/MockupContent/index.tsx
@@ -0,0 +1,20 @@
+import styles from "./styles.module.scss";
+
+function MockupContent() {
+  return (
+    <>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+      <div className={styles.content}>MockupContent</div>
+    </>
+  );
+}
+
+export default MockupContent;
