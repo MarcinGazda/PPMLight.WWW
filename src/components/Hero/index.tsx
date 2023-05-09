3f9d8abe52981af40b68008fabb34c555a02a2d3
HubertKorcala <korcalahubert@gmail.com>
2023-05-09 13:29:18 +0200

    feat: add scaling in Hero component

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -3,10 +3,32 @@ import HeroImg from "../../assets/hero.png";
 import facebook from "../../assets/facebook.png";
 import twitter from "../../assets/twitter.png";
 import instagram from "../../assets/instagram.png";
+import { useEffect, useState } from "react";
 
 const Hero = () => {
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const maxValue = 1455;
+  const minValue = 768;
+  useEffect(() => {
+    const handleResize = () => {
+      setScreenWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const calculatePercentage = (width: number) => {
+    return width > minValue ? width / maxValue : 1;
+  };
+
+  const scaleStyle = { scale: `${calculatePercentage(screenWidth)}` };
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={scaleStyle}>
       <div className={styles.socialMedia}>
         <div className={styles.circle}>
           <img className={styles.icon} src={twitter} alt="twitter icon" />
