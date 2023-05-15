194cde7c81c1fad10511ee7b431aacd10c800dbc
HubertKorcala <korcalahubert@gmail.com>
2023-05-15 22:41:08 +0200

    feat: add background in Hero

--- src/views/Hero/index.tsx
+++ src/views/Hero/index.tsx
@@ -3,6 +3,7 @@ import HeroImg from "../../assets/hero.png";
 import facebook from "../../assets/icons/facebook.png";
 import twitter from "../../assets/icons/twitter.png";
 import instagram from "../../assets/icons/instagram.png";
+import BackgroundImg from "../../assets/background/background-1.png";
 import { useEffect, useState } from "react";
 
 const Hero = () => {
@@ -28,30 +29,41 @@ const Hero = () => {
   const scaleStyle = { scale: `${calculatePercentage(screenWidth)}` };
 
   return (
-    <div className={styles.container} style={scaleStyle}>
-      <div className={styles.socialMedia}>
-        <div className={styles.circle}>
-          <img className={styles.icon} src={twitter} alt="twitter icon" />
-        </div>
-        <div className={styles.circle}>
-          <img className={styles.icon} src={facebook} alt="facebook icon" />
-        </div>
-        <div className={styles.circle}>
-          <img className={styles.icon} src={instagram} alt="instagram icon" />
-          <span className={styles.follow}>Follow us:</span>
+    <section>
+      <div className={styles.background}>
+        <img className={styles.imgBackground} src={BackgroundImg} alt="" />
+        <div className={styles.container} style={scaleStyle}>
+          <div className={styles.socialMedia}>
+            <div onClick={() => alert("twitter")} className={styles.circle}>
+              <img className={styles.icon} src={twitter} alt="twitter icon" />
+            </div>
+            <div onClick={() => alert("facebook")} className={styles.circle}>
+              <img className={styles.icon} src={facebook} alt="facebook icon" />
+            </div>
+            <div onClick={() => alert("instagram")} className={styles.circle}>
+              <img
+                className={styles.icon}
+                src={instagram}
+                alt="instagram icon"
+              />
+            </div>
+            <span className={styles.follow}>Follow us:</span>
+          </div>
+          <div className={styles.text}>
+            <h1
+              className={styles.title}
+            >{`The biggest platform\nfor your growth.`}</h1>
+            <p
+              className={styles.description}
+            >{`There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.`}</p>
+            <button className={styles.button}>Read More</button>
+          </div>
+          <div className={styles.imgWrapper}>
+            <img className={styles.img} src={HeroImg} alt="" />
+          </div>
         </div>
       </div>
-      <div className={styles.text}>
-        <h1
-          className={styles.title}
-        >{`The biggest platform\nfor your growth.`}</h1>
-        <p
-          className={styles.description}
-        >{`There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form.`}</p>
-        <button className={styles.button}>Read More</button>
-      </div>
-      <img className={styles.img} src={HeroImg} alt="" />
-    </div>
+    </section>
   );
 };
 
